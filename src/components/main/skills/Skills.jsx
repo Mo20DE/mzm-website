77b664e25ba7e6a801427d65699e796bd944b7fe
Mohammed Zain Maqsood <mzm@macbookair.speedport.ip>
2025-03-16 21:45:00 +0100

    changed animation in Skills.jsx & Contact.jsx

--- src/components/main/skills/Skills.jsx
+++ src/components/main/skills/Skills.jsx
@@ -17,7 +17,7 @@ const Skills = () => {
             initial={{y: 20, opacity: 0}}
             whileInView={{y: 0, opacity: 1}}
             transition={{duration: 0.8, ease: "easeOut"}}
-            viewport={{once: true, amount: 0.25}}
+            viewport={{once: true, amount: 0.3}}
         >
             <h1 className='
                 relative text-4xl lg:text-5xl 
