15a8e79407a3717fbc6fa8e00269bde9fab28265
Mohammed Zain Maqsood <mzm@macbookair.speedport.ip>
2025-03-16 15:13:23 +0100

    minor change in footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
             w-full h-auto p-8 gap-y-5 bg-gradient-to-r from-gray-400 
             via-gray-300 to-gray-300 text-black dark:bg-gradient-to-r 
             dark:from-gray-900 dark:via-gray-800 dark:to-gray-800 
-            dark:text-white border-t-[0.5px] border-t-gray-500
+            dark:text-white border-t-[0.5px] border-t-gray-600
         `}>
             <div className="">
                 <Socials size={35} />
